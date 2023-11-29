a5b4cccfd7989d819df7d09b28ac62069f7cc4c2
Abhishek-Hosamani <01fe20bcs088@kletech.ac.in>
2023-11-30 01:42:59 +0530

    Check

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const twilio = require('twilio');
 const accountSid = 'AC38143c2e07dae337c0ee3a6cbdb6ccdc';
-const authToken = '52c3d06843e11a2314b9e091e47164a4';
+const authToken = '7220aacceb319a5ba99aa0b688d2fa44';
 const client = new twilio(accountSid, authToken);
 
 function generateOTP() {
@@ -14,28 +14,28 @@ app.get('/', (req, res) => {
     res.send('This is IS app')
 })
 app.get('/test', (req, res) => {
-    // const width = 200;
-    // const height = 100;
-    // const { image } = captcha(width, height);
-    // const phoneNumber = '+91 6362197359';
-    // const otp = generateOTP();
+    const width = 200;
+    const height = 100;
+    const { image } = captcha(width, height);
+    const phoneNumber = '+91 6362197359';
+    const otp = generateOTP();
 
 
-    // client.messages.create({
-    //     body: `Your OTP is: ${otp}`,
-    //     to: phoneNumber,
-    //     from: '+1 720 466 7552' // Your Twilio phone number
-    // })
-    //     .then(message => {
-    //         console.log(`OTP sent to ${phoneNumber}: ${message.sid}`);
+    client.messages.create({
+        body: `Your OTP is: ${otp}`,
+        to: phoneNumber,
+        from: '+1 720 466 7552' // Your Twilio phone number
+    })
+        .then(message => {
+            console.log(`OTP sent to ${phoneNumber}: ${message.sid}`);
 
-    //     })
-    //     .catch(error => {
-    //         console.error(`Error sending OTP: ${error.message}`);
+        })
+        .catch(error => {
+            console.error(`Error sending OTP: ${error.message}`);
 
-    //     });
-    res.send("Hii")
-    // res.render('form.ejs', { image });
+        });
+    // res.send("Hii")
+    res.render('form.ejs', { image });
 });
 
 // Captcha generation, returns PNG data URL and validation text
